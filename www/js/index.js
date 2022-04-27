34cd49598626fb16564e033db72490add02c5205
Joaquin Anduano <joaquin@techlab.com.uy>
2022-04-27 14:56:22 -0300

    fix camera

--- www/js/index.js
+++ www/js/index.js
@@ -26,5 +26,5 @@ function onDeviceReady() {
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
-    intercom.registerIdentifiedUser({userId: "123456"});
+    intercom.registerForPush();
 }
